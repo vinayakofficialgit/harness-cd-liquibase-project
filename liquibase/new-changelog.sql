2ba4a5864ef9c441e75322eacbece4daa632bd87
vinayakofficialgit <153804564+vinayakofficialgit@users.noreply.github.com>
2025-02-04 10:45:18 +0530

    Update new-changelog.sql

--- liquibase/new-changelog.sql
+++ liquibase/new-changelog.sql
@@ -11,13 +11,14 @@ CREATE TABLE IF NOT EXISTS person (
 
 --changeset your.vinayaks:12 labels:example-label context:example-context
 --comment: example comment
-create table company (
-    id int primary key auto_increment not null,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
+CREATE TABLE IF NOT EXISTS company (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    address1 VARCHAR(50),
+    address2 VARCHAR(50),
+    city VARCHAR(30)
 );
+
 --rollback DROP TABLE company;
 
 --changeset other.DevOps:13 labels:example-label context:example-context
