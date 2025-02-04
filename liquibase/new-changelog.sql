b00b43675967b6033ff0295f16887db0d7aad980
vinayakofficialgit <153804564+vinayakofficialgit@users.noreply.github.com>
2025-02-04 10:43:04 +0530

    Update new-changelog.sql

--- liquibase/new-changelog.sql
+++ liquibase/new-changelog.sql
@@ -1,11 +1,11 @@
 --liquibase formatted sql
 --changeset your.linuxuser:11        
-create table person (
-    id int primary key auto_increment not null,
-    name varchar(50) not null,
-    address1 varchar(50),
-    address2 varchar(50),
-    city varchar(30)
+CREATE TABLE IF NOT EXISTS person (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    address1 VARCHAR(50),
+    address2 VARCHAR(50),
+    city VARCHAR(30)
 );
 --rollback DROP TABLE person;
 
